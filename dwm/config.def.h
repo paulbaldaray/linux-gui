e62fea40f2caa35877c09a36344829e22f26a859
Paul Baldaray <paulbaldaray@gmail.com>
2020-06-24 17:36:50 -0700

    Updated key bindings

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -79,21 +79,21 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,          togglebar,      {0} },
 	{ MODKEY,                       XK_j,          focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,          focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_m,          incnmaster,     {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_m,          incnmaster,     {.i = -1 } },
+	/* { MODKEY,                       XK_m,          incnmaster,     {.i = +1 } }, */
+	/* { MODKEY|ShiftMask,             XK_m,          incnmaster,     {.i = -1 } }, */
 	{ MODKEY,                       XK_h,          setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,          setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return,     zoom,           {0} },
 	{ MODKEY,                       XK_Tab,        view,           {0} },
 	{ MODKEY,                       XK_c,          killclient,     {0} },
 	{ MODKEY,                       XK_space,      setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,      togglefloating, {0} },
+	{ MODKEY,                       XK_m,          togglefloating, {0} },
 	{ MODKEY,                       XK_0,          view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,          tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,      focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period,     focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,      tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period,     tagmon,         {.i = +1 } },
+	/* { MODKEY,                       XK_comma,      focusmon,       {.i = -1 } }, */
+	/* { MODKEY,                       XK_period,     focusmon,       {.i = +1 } }, */
+	/* { MODKEY|ShiftMask,             XK_comma,      tagmon,         {.i = -1 } }, */
+	/* { MODKEY|ShiftMask,             XK_period,     tagmon,         {.i = +1 } }, */
 	TAGKEYS(                        XK_1,                          0)
 	TAGKEYS(                        XK_2,                          1)
 	TAGKEYS(                        XK_3,                          2)
@@ -116,6 +116,12 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,          spawn,          SHCMD("firefox") },
 	{ MODKEY|ShiftMask,             XK_f,          spawn,          SHCMD("firefox --private-window") },
 	{ MODKEY,                       XK_v,          spawn,          SHCMD("pavucontrol") },
+	{ MODKEY,                       XK_comma,      spawn,          SHCMD("sudo backlight -100") },
+	{ MODKEY,                       XK_period,     spawn,          SHCMD("sudo backlight 100") },
+	{ MODKEY|ShiftMask,             XK_comma,      spawn,          SHCMD("sudo backlight -10") },
+	{ MODKEY|ShiftMask,             XK_period,     spawn,          SHCMD("sudo backlight 10") },
+	{ MODKEY|ShiftMask|ControlMask, XK_comma,      spawn,          SHCMD("sudo backlight -1") },
+	{ MODKEY|ShiftMask|ControlMask, XK_period,     spawn,          SHCMD("sudo backlight 1") },
 };
 
 /* button definitions */
