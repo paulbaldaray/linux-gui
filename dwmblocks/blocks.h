dbe476bd7accad6d4e166df8bd9271d736b8c898
Paul Baldaray <paulbaldaray@gmail.com>
2020-07-07 16:04:16 -0700

    Added dwmblocks

---
+++ dwmblocks/blocks.h
@@ -0,0 +1,12 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+
+#define SCRIPT "/home/paul/suckless/dwmblocks/scripts/"
+
+static const Block blocks[] = {
+	/*Icon | Command                    | Interval | Signal*/
+	{"",     SCRIPT"date",                      1,         0},
+	{"",    SCRIPT"battery",             60,        0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
