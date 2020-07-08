daf0f583c15ecd6d6513db3d73f4d58e72c45cbf
Paul Baldaray <paulbaldaray@gmail.com>
2020-07-08 07:29:01 -0700

    Spacing

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -4,8 +4,8 @@
 
 static const Block blocks[] = {
 	/*Icon | Command                    | Interval | Signal*/
-	{"",     SCRIPT"date",                      1,         0},
-	{"",    SCRIPT"battery",             60,        0},
+	{"",     SCRIPT"date",                1,         0},
+	{"",     SCRIPT"battery",             60,        0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
